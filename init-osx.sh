7189f7ab78565173c5bc608ce42a9c3677007629
Alex Gotev <alex@gotev.net>
2016-06-29 17:13:24 +0200

    updated OS X init script

--- init-osx.sh
+++ init-osx.sh
@@ -5,3 +5,8 @@ sudo npm install --unsafe-perm=true -g ios-sim
 sudo npm install --unsafe-perm=true -g ios-deploy
 npm install
 
+echo 'Bugsnag.apiKey = "YOUR-API-KEY-HERE";' > www/js/bugsnag_apikey.js
+echo 'window.ANDROID_SENDER_ID = "YOUR-ANDROID-ID-HERE";' > www/shared/js/android_sender_id.js
+
+cordova prepare
+cordova build ios
