6aae1bcbb400e1b21de2cd3cbd0ee04b9f4f4b83
Alex Gotev <alex@gotev.net>
2016-06-29 17:59:46 +0200

    added instructions for the first initilization in XCode

--- open-xcode-project.sh
+++ open-xcode-project.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+echo "PAY ATTENTION!!!"
+echo
+echo "In the XCode project change the Bundle identifier to: com.voismart.chat"
+echo "then select VoiSmart Srl as Team and Fix Provisioning profile by clicking on the button"
+echo
+
 open platforms/ios/Rocket.Chat.xcodeproj/ 
