3affe3d2b4a99d5f1f01352f648fc7feb181dd81
Alex Gotev <alex@gotev.net>
2016-07-05 16:04:11 +0200

    set sender id

--- branding.sh
+++ branding.sh
@@ -2,6 +2,7 @@
 if [ "x$1" == "xtim" ]
 then
     echo "Branding TIM"
+    echo "window.ANDROID_SENDER_ID = \"746061423072\";" > www/shared/js/android_sender_id.js
     sed -i '' 's/com.voismart.chat/it.telecomitalia.chat/g' config.xml
     sed -i '' 's/VoiSmart Chat mobile application/TIM chat/g' config.xml
     sed -i '' 's@http://www.voismart.it@http://www.tim.it@g' config.xml
