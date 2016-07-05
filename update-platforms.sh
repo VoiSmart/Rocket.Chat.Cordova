65ad962a3775fbcf0fb5b69eec10ae9f711b7262
Alex Gotev <alex@gotev.net>
2016-07-05 11:21:20 +0200

    added GCM fix also to update-platforms script

--- update-platforms.sh
+++ update-platforms.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+#Fix GCM and Google Plus dependencies in cordova plugins
+#Check: https://github.com/RocketChat/Rocket.Chat.Cordova/issues/118
+sed -i -e 's/com.google.android.gms:play-services-plus:+/com.google.android.gms:play-services-plus:9.0.2/g' plugins/cordova-plugin-googleplus/plugin.xml
+sed -i -e 's/com.google.android.gms:play-services-identity:+/com.google.android.gms:play-services-identity:9.0.2/g' plugins/cordova-plugin-googleplus/plugin.xml
+sed -i -e 's/com.google.android.gms:play-services-gcm:+/com.google.android.gms:play-services-gcm:9.0.2/g' plugins/phonegap-plugin-push/plugin.xml
+
 cordova platform remove android
 cordova platform remove ios
 cordova platform add android
